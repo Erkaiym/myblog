552efdc0d0f5fd8e24c748e900fe385fe1e4d1e3
Erkaiym <akylova.erkaiym@gmail.com>
2019-04-11 00:01:49 +0600

    added static root

--- blogengine/settings_prod.py
+++ blogengine/settings_prod.py
@@ -2,12 +2,14 @@ from .settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'simpledjango',
         'USER': 'simpledjangouser',
         'PASSWORD': '123simpledjango123',
         'HOST': 'localhost',
-        'PORT': ",
+        'PORT': '',
     }
 }
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
